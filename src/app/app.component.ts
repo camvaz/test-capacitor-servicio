a3333d03996cf0bbb8c45d49aabf1c8d15316ce5
Victor Campos <camposvazquezv@gmail.com>
2020-10-23 18:56:57 -0500

    inicial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
 })
 export class AppComponent {
-  title = 'test-audio';
+  title = "test-audio";
+  utterTest: SpeechSynthesisUtterance;
+  voices: SpeechSynthesisVoice[];
+  synth: SpeechSynthesis;
+
+  ngOnInit() {
+    this.synth = window.speechSynthesis;
+    setTimeout(() => {
+      this.voices = this.synth.getVoices();
+      this.utterTest = new SpeechSynthesisUtterance("Testing");
+      this.utterTest.voice = this.voices[48];
+      this.synth.speak(this.utterTest);
+      console.log(this.voices);
+    }, 10);
+    console.log(this.voices, this.synth);
+    // this.utterTest = new SpeechSynthesisUtterance("Testing");
+  }
 }
